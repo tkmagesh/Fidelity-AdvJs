aff118c11ea4a79f9277af078896ca27c9be701e
Magesh <tkmagesh77@gmail.com>
2013-07-20 16:12:12 +0530

    View to a new file

---
+++ Day-4/SoCDemo/SalaryCalculatorView.js
@@ -0,0 +1,51 @@
+//Not the right way of writing JavaScript classes... 
+//Please follow the Object-ConstructorFunction-Prototype Model
+
+function SalaryCalculatorView(model){
+	this.model =  model;
+	this.initialize = function(){
+		model.subscribers.add('net',function(){
+			$("#divSalary").text(model.net());
+		});
+		model.subscribers.add('tax',function(){
+			$("#rangeTax").val(model.tax());
+			$("#spanTax").text(model.tax());
+		});
+		model.subscribers.add('basic',function(){
+			$("#txtBasic").val(model.basic());
+		});
+		model.subscribers.add('hra',function(){
+			$("#txtHra").val(model.hra());
+		});
+		model.subscribers.add('da',function(){
+			$("#txtDa").val(model.da());
+		});
+		this.root = $("#calculatorTemplate").html();
+		this.$root = $(this.root);
+		this.$root.on('change',"#txtBasic", function(){
+			if (this.value !== ''){
+				model.basic(parseInt(this.value));
+				model.calculate();
+			}
+		});
+		this.$root.on('change',"#txtHra", function(){
+			if (this.value !== ''){
+				model.hra(parseInt(this.value));
+				model.calculate();
+			}
+		});		
+		this.$root.on('change',"#txtDa", function(){
+			if (this.value !== ''){
+				model.da(parseInt(this.value));
+				model.calculate();
+			}
+		});
+		this.$root.on('change',"#rangeTax", function(){
+			if (this.value !== ''){
+				model.tax(parseInt(this.value));
+				model.calculate();
+			}
+		});						
+	}
+	
+}
